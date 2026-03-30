de1d8279c129a759a9d6faa1f39272aff161ace5
Mélanie Bruzac <130762906+Melb56@users.noreply.github.com>
2026-03-30 18:10:45 +0200

    Update job title from 'Développeuse front-end' to 'Développeuse Web'

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -8,7 +8,7 @@ const about = () => {
     <section className='about' id='about'>
         <h2 data-aos ='fade-left' data-aos-duration='1000'>À Propos De Moi</h2>
         <p className='about-text' data-aos ='fade-left'>
-Développeuse front-end spécialisée en React / Next.js et WordPress, je conçois des interfaces et applications web performantes en combinant expérience utilisateur, performance technique et SEO.<br />
+Développeuse Web spécialisée en React / Next.js et WordPress, je conçois des interfaces et applications web performantes en combinant expérience utilisateur, performance technique et SEO.<br />
 Je développe des applications structurées, j’intègre des API et je veille à la qualité d’intégration front/back afin de livrer des solutions fiables et maintenables. <br />
 À travers mes projets, j’ai travaillé sur l’authentification, la structuration modulaire des composants, l’optimisation des performances (Core Web Vitals) et la personnalisation de sites WordPress orientés performance et référencement. <br />
 Habituée aux environnements JavaScript modernes, je m’adapte rapidement aux contextes agence, ESN ou startup, avec une attention constante portée à la qualité du code, à la maintenabilité et à l’efficacité des livraisons. <br />
