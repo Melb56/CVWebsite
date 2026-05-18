2a55f07fc3b5089139f4b698b1e654216c8e4a35
Mélanie Bruzac <130762906+Melb56@users.noreply.github.com>
2026-05-18 11:34:13 +0200

    Update contact information and job interest

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -8,9 +8,8 @@ const Contact = () => {
     <section id='contact'>
         <h2 data-aos='fade-down' data-aos-duration='1500'>Contactez-moi !</h2>
         <div className='intro-contact' data-aos='fade-right' data-aos-duration='1500'>
-          <p >À la recherche d’un poste de</p>
-          <p className='job'>Développeuse Front-end </p>   
-          <p >pour concevoir des interfaces modernes, contribuer à des projets concrets et m’impliquer au sein d’une agence, d’une ESN ou d’une startup.</p>
+          <p >À la recherche d’une alternance de 12 ans en </p>
+          <p className='job'>Cybersécurité </p> 
           <div className='text-contact' ><p> Je serais ravie d’échanger avec vous de mon parcours et ma motivation.</p></div>
         </div>
 
